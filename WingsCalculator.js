357133e03c2cea0b9c7b6cd72ae11fef385c18f5
KJ-519 <kward21@cps.edu>
2025-05-29 16:16:01 +0000

    V1

--- WingsCalculator.js
+++ WingsCalculator.js
@@ -5,17 +5,17 @@ let result = document.querySelector(".result");
 
 
 // 1. Convert to an arrow function.
-const calculateDoggieAge = humanAge => {
+const calculateWingstopAge = humanAge => {
  
 
   // 2. Declare a doggieAge variable and set it equal to the humanAge parameter multiplied by 7.
 
-  let doggieAge = humanAge * 7;
+  let WingsAge = humanAge * 1;
   
 
 
-  // 3. Update the result div's inner html to display the dog's age. Use string interpolation to create the message: "Your doggie is _________ years old in human years!"
-result.innerHTML = "Your doggie is " + doggieAge + " years old in human years"
+  // 3. Update the result div's inner html to display the dog's age. Use string interpolation to create the message: "Wingstop is _________ years old!"
+result.innerHTML = "Wingstop is " + WingsAge + " years old"
   
   
 }
@@ -27,8 +27,8 @@ equalsButton.addEventListener("click", () => {
   // 4. Declare a userInput variable and set it equal to the value of the input. This needs to be a number, not a string! Be sure to place that inside our Number() method!
  let userInput = Number(input.value);
 
-  // 5. Call the calculateDoggieAge function. Be sure to include the userInput variable as the parameter!
-calculateDoggieAge(userInput);
+  // 5. Call the calculateWingstopAge function. Be sure to include the userInput variable as the parameter!
+calculateWingstopAge(userInput);
 
 
 })
